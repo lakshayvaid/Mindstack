bcb15db2d3ed56fcc5a41834606d8b6006fc55c6
lakshay vaid <lakshayvaid21@gmail.com>
2025-07-06 01:39:40 +0530

    changed frontend url in backend

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,4 +3,4 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 export const JWT_SECRET=process.env.JWT_SECRET!;
-export const Frontend_url=process.env.FRONTEND_URL!;
+export const Frontend_url='https://mindstackapp.vercel.app';
